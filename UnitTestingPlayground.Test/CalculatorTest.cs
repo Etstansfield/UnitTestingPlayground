1288959ad4344110478f457e6e789aa60a5c9c4e
Etstansfield <etstansfield@hotmail.com>
2020-09-29 19:50:09 +0100

    First Commit
    Start of a unit testing playground to mess around with unit testing in XUnit

---
+++ UnitTestingPlayground.Test/CalculatorTest.cs
@@ -0,0 +1,88 @@
+using System;
+using Xunit;
+using UnitTestingPlayground;
+using System.Diagnostics;
+
+namespace UnitTestingPlayground.Test
+{
+    public class CalculatorTest
+    {
+        [Theory]
+        [InlineData(8,4,12)]
+        [InlineData(3,2,5)]
+        [InlineData(1.25,7.75,9)]
+        public void Add_SimpleTest(double x, double y, double expected)
+        {
+            // Arrange
+            
+            // Act
+            double actual = Calculator.Add(x, y);
+            // Assert
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Add_MaxDoubleTest()
+        {
+            // Arrange
+
+            // Act
+            double actual = Calculator.Add(double.MaxValue, double.MaxValue);
+            // Assert
+
+            Assert.True(double.IsInfinity(actual));
+        }
+
+        [Fact]
+        public void Add_MinDoubleTest()
+        {
+            // Arrange
+
+            // MinValue
+            double actual = Calculator.Add(double.MinValue, double.MinValue);
+            // Assert
+
+            Assert.True(double.IsInfinity(actual));
+        }
+
+        [Theory]
+        [InlineData(8, 4, 4)]
+        [InlineData(3, 2, 1)]
+        [InlineData(1.25, 7.75, -6.5)]
+        public void Subtract_SimpleTest(double x, double y, double expected)
+        {
+            // Arrange
+
+            // Act
+            double actual = Calculator.Subtract(x, y);
+            // Assert
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Subtract_MaxDoubleTest()
+        {
+            // Arrange
+            double expected = 0;
+            // Act
+            double actual = Calculator.Subtract(double.MaxValue, double.MaxValue);
+            // Assert
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Subtract_MinDoubleTest()
+        {
+            // Arrange
+            double expected = 0;
+            // MinValue
+            double actual = Calculator.Subtract(double.MinValue, double.MinValue);
+            // Assert
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ UnitTestingPlayground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestingPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Subtract(double.MaxValue, double.MaxValue));
+        }
+    }
+}
