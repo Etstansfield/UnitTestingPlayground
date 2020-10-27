b8dc05668e502cb1fc1d47165ca9c020dba55299
Etstansfield <etstansfield@hotmail.com>
2020-10-27 22:27:09 +0000

    Async
    Start of async unit testing

---
+++ UnitTestingPlayground.Test/DummyAsyncTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTestingPlayground.Test
+{
+    public class DummyAsyncTest
+    {
+        [Fact]
+        public static async Task AsyncTimer_Success()
+        {
+            await DummyAsync.AsyncTimer();
+
+            Assert.True(true);
+        }
+
+    }
+}
---
+++ UnitTestingPlayground/DummyAsync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingPlayground
+{
+    public class DummyAsync
+    {
+
+        public static async Task AsyncTimer()
+        {
+            await Task.Delay(1000);
+        }
+
+    }
+}
